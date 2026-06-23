1c8bd680d4f3da0d36fd4a4171489fd348d780c7
Emilakesofficial <realemilakes@gmail.com>
2026-06-22 23:36:18 -0700

    fix: terraform formatting

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket =  "tf-state-github-actions-demo-adekunle"
-        key = "terraform/state/terraform.tfstate"
-        region = "us-east-1"
-        use_lockfile = true
-        encrypt = true
-    }
+  backend "s3" {
+    bucket       = "tf-state-github-actions-demo-adekunle"
+    key          = "terraform/state/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
